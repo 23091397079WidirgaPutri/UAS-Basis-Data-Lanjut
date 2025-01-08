428114ed970dfb61737605dcb7df1ccb8548d27c
23091397079 Widirga Putri A.W <149134707+23091397079WidirgaPutri@users.noreply.github.com>
2025-01-08 22:02:15 +0700

    Update show.blade.php

--- resources/views/Posts/show.blade.php
+++ resources/views/Posts/show.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
@@ -20,5 +21,6 @@
             <a href="{{ route('posts.index') }}" class="btn btn-secondary">Kembali ke Daftar Postingan</a>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
